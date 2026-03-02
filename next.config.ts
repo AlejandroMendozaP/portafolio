a6c3f0b8e7ab3d673252b2701999b1dad1d927b4
AlejandroMendozaP <alemenpa2002@gmail.com>
2026-03-01 22:13:41 -0600

    feat: add basePath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  //basePath: '/portafolio',
+  basePath: '/portafolio',
   images: {
     unoptimized: true,
   },
